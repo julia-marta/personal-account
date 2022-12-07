ceec5ec4dabea02936bd459b4972ef43024d3648
Julia Nabiulina <marta237@yandex.ru>
2022-12-07 17:25:35 +0300

    update headers

--- server.js
+++ server.js
@@ -1,52 +1,55 @@
-const jsonServer = require('json-server')
-const path = require('path');
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+const jsonServer = require("json-server");
+const path = require("path");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults({
-  static: './build'
-})
+  static: "./build",
+});
 const db = router.db;
 
 const port = process.env.PORT || 5000;
 
-server.use(middlewares)
-server.use(jsonServer.bodyParser)
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
 
 module.exports = (req, res, next) => {
-  res.header('Content-Type', 'application/json')
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS')
-  next()
-}
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+  );
+  next();
+};
 
-server.get('/:page', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+server.get("/:page", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
 });
 
-server.post('/api/users', (req, res) => {
-  const {login, password} = req.body;
+server.post("/api/users", (req, res) => {
+  const { login, password } = req.body;
   const users = db.getState().users;
 
-  const registeredUser = users.filter(user => user.login === login);
+  const registeredUser = users.filter((user) => user.login === login);
 
   if (!registeredUser.length) {
     return res.status(404).jsonp({
-      loginError: "This user is not registered"
-    })
+      loginError: "This user is not registered",
+    });
   }
 
   const isPasswordCorrect = registeredUser[0].password === password;
 
   if (!isPasswordCorrect) {
     return res.status(404).jsonp({
-      passwordError: "Password is not correct"
-    })
+      passwordError: "Password is not correct",
+    });
   }
 
-  return res.status(200).jsonp(registeredUser)
-})
+  return res.status(200).jsonp(registeredUser);
+});
 
-server.use('/api', router)
+server.use("/api", router);
 server.listen(port, () => {
-  console.log('JSON Server is running')
-})
+  console.log("JSON Server is running");
+});
